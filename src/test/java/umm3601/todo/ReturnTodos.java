bac30e5386ac491d1435af6fbf0167f1bf631d01
Shamund Gordon <stgordon19@gmail.com>
2017-01-30 20:25:55 -0600

    Created a package for our todo tests. Began to work on the tasks

---
+++ src/test/java/umm3601/todo/ReturnTodos.java
@@ -0,0 +1,18 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by gordo580 on 1/30/17.
+ */
+public class ReturnTodos {
+
+    @Test
+    public void totalTodoCount throws IOException{
+        TodoController todoController = new TodoController();
+
+
+    }
+}

57491506c881787853c9d0e41be386d6ba4e6d1c
Shamund Gordon <stgordon19@gmail.com>
2017-02-01 11:07:04 -0600

    Done with ReturnToDos but should be reviewed

--- src/test/java/umm3601/todo/ReturnTodos.java
+++ src/test/java/umm3601/todo/ReturnTodos.java
@@ -1,5 +1,6 @@
 package umm3601.todo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -7,9 +8,6 @@ import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by gordo580 on 1/30/17.
- */
 public class ReturnTodos {
 
     @Test
@@ -18,8 +16,15 @@ public class ReturnTodos {
         Todo[] allTodos = todoController.listTodos(new HashMap<>());
         //We should find out the number of todos we actually have
         assertEquals("Incorrect number of users", 300, allTodos.length);
+    }
 
-
+    @Test
+    public void returnFirstTodoInFullList() throws IOException {
+        TodoController todoController = new TodoController();
+        Todo[] allTodos = todoController.listTodos(new HashMap<>());
+        Todo firstTodo = allTodos[0];
+        assertEquals("Incorrect Owner", "Blanche", firstTodo.owner);
+        assertEquals("Incorrect Category", "software design", firstTodo.category);
     }
 }
 

999a8e777ee3ed8f0abadca55d6a5f968d6d07b0
Shamund Gordon <stgordon19@gmail.com>
2017-02-01 12:01:07 -0600

    Finished testing code for limiting the number of todos as specified

--- src/test/java/umm3601/todo/ReturnTodos.java
+++ src/test/java/umm3601/todo/ReturnTodos.java
@@ -15,7 +15,7 @@ public class ReturnTodos {
         TodoController todoController = new TodoController();
         Todo[] allTodos = todoController.listTodos(new HashMap<>());
         //We should find out the number of todos we actually have
-        assertEquals("Incorrect number of users", 300, allTodos.length);
+        assertEquals("Incorrect number of todos", 300, allTodos.length);
     }
 
     @Test
@@ -25,6 +25,20 @@ public class ReturnTodos {
         Todo firstTodo = allTodos[0];
         assertEquals("Incorrect Owner", "Blanche", firstTodo.owner);
         assertEquals("Incorrect Category", "software design", firstTodo.category);
+    }
+
+    @Test
+    public void returnSevenTodos() throws IOException{
+        TodoController todoController = new TodoController();
+        Todo[] sevenTodos = todoController.setNumberOfTodos(new HashMap<>(), 7);
+        assertEquals("Incorrect number of todos", 7, sevenTodos.length);
+    }
+
+    @Test
+    public void returnOneHundredTodos() throws IOException{
+        TodoController todoController = new TodoController();
+        Todo[] oneHundredTodos = todoController.setNumberOfTodos(new HashMap<>(), 100);
+        assertEquals("Incorrect Number Of Todos", 100, oneHundredTodos.length);
     }
 }
 

8d788df0439c25c48516084a655f1d6d73df36a4
Shamund Gordon <stgordon19@gmail.com>
2017-02-01 12:06:17 -0600

    Created Todo object and TodoController Object

---
+++ src/main/java/umm3601/todo/Todo.java
@@ -0,0 +1,12 @@
+package umm3601.todo;
+
+/**
+ * Created by gordo580 on 2/1/17.
+ */
+public class Todo {
+    String _id;
+    String owner;
+    boolean status;
+    String body;
+    String category;
+}
---
+++ src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,46 @@
+package umm3601.todo;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Created by gordo580 on 2/1/17.
+ */
+public class TodoController {
+
+    private Todo[] todos;
+
+    public TodoController() throws IOException{
+        Gson gson = new Gson();
+        FileReader reader = new FileReader("src/main/data/todos.json");
+        todos = gson.fromJson(reader, Todo[].class);
+    }
+
+    public Todo[] listTodos(Map<String, String[]> queryParameter){
+        Todo[] filteredTodos = todos;
+
+        return filteredTodos;
+    }
+
+    // Returns a Single one
+    public Todo getTodo(String id){
+        return Arrays.stream(todos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+    }
+
+    //Return specified number of todos
+    public Todo[] setNumberOfTodos(Map<String, String[]> queryParameter, int listLimit){
+        Todo[] limitedList = new Todo[listLimit];
+
+        for(int i = 0; i < listLimit; i++){
+            limitedList[i] = todos[i];
+        }
+
+        return limitedList;
+    }
+
+
+}

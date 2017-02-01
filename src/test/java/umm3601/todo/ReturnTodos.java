a679df7a3d8b4bd541773993b5dc336be0bfe53c
Shamund Gordon <stgordon19@gmail.com>
2017-02-01 10:55:33 -0600

    Managed to get the first test to pass, which counts the number of todos we have to be sure we're properly reading the todo file correctly. Though, we don't know how many todo's we should have.

--- src/test/java/umm3601/todo/ReturnTodos.java
+++ src/test/java/umm3601/todo/ReturnTodos.java
@@ -3,6 +3,9 @@ package umm3601.todo;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by gordo580 on 1/30/17.
@@ -10,9 +13,13 @@ import java.io.IOException;
 public class ReturnTodos {
 
     @Test
-    public void totalTodoCount throws IOException{
+    public void totalTodoCount() throws IOException{
         TodoController todoController = new TodoController();
+        Todo[] allTodos = todoController.listTodos(new HashMap<>());
+        //We should find out the number of todos we actually have
+        assertEquals("Incorrect number of users", 300, allTodos.length);
 
 
     }
 }
+
